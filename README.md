9cbbe981396cf5dec13c791f2b2475e40ef7b4cd
哥斯拉不说话 <994019222@qq.com>
2022-08-13 22:46:05 +0800

    完成vuex-todos的开发与优化部署

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
 # vuex-todos
 
-## Project setup
-```
-npm install
-```
+![](img/01.png)
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## 步骤①: 启动前端服务
 
-### Compiles and minifies for production
-```
-npm run build
-```
+> - 首先安装vue-cli脚手架，直接在任意目录执行`npm install vue-cil@3 -g `对应版本为3系列
+> - 然后使用`vue -V`查看版本
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+![](img/05.png)
+
+`clone`或`download`项目后, 直接在对应的目录执行`npm install`, 直接把前端服务所需要的依赖全部安装, 然后再执行对应的`npm run serve`即可启动前端服务
+
+![](img/03.png)
+
+
+
+## 步骤②: 本地打包浏览
+
+### 1. 将`vuex-todos-server`解压
+
+### 2. npm install安装依赖
+
+![](img/04.png)
+
+### 3. 启动服务端  node app.js
+
+![](img/02.png)
+
+> - 也可以自行在`vue ui`界面或者本地`vue-shop`路径下通过`npm run build`命令进行打包,将打包后的`dist`文件夹复制到`vue-shop-server`文件夹中即可
+
+### 4. 浏览器打开`app.js`配置地址http://127.0.0.1:68浏览项目
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,40 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // 通过chainWebpack自定义打包入口
+  chainWebpack: config => {
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production', config => {
+      // entry找到默认的打包入口，调用clear则是删除默认的打包入口
+      // add添加新的打包入口
+      config.entry('app').clear().add('./src/main-prod.js')
+
+      // 通过externals加载外部CDN资源
+      config.set('externals', {
+        //包名：全局变量
+        vue: 'Vue',
+        axios: 'axios',
+        'ant-design-vue': 'antd'
+      })
+      // 通过插件定制首页内容
+      config.plugin('html').tap(args => {
+        args[0].isProd = true
+        return args
+      })
+    })
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+      // 通过插件定制首页内容
+      config.plugin('html').tap(args => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  },
+  transpileDependencies: true,
+  configureWebpack: {
+    externals: 'hls.js'
+  }, // 在这配置webpack的externals这个字段
 })
